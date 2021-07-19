ada7e285a08debff1ac4aaff78a6af3eb806c3d2
Aman Kumar Singh <62831134+maskmanlucifer@users.noreply.github.com>
2021-07-19 10:13:34 +0530

    Add files via upload

---
+++ userInput.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+
+echo "Please Enter Your Full Name"
+
+read FNAME LNAME 
+
+echo "Your Name is $FNAME $LNAME"
---
+++ variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+NAME="Aman"
+
+echo "My Name is $NAME"
+
+SPORT="Foot"
+
+echo "The Most popular sport is ${SPORT}ball"
+
+# You can not have any spaces between variable name and equal sign and variable value
+# Variable names should be in UPPERCASE
+# Dealing with variables is pretty much neat with curly braces
+# Most of the variable naming conventions are similar to C++
